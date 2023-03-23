fc910493ae6797fe4434106fb13713c63ccf657f
SubhrajyotiSen <subhrajyoti12@gmail.com>
2023-03-24 01:59:12 +0530

    Split into starter and final code

---
+++ source-code/starter-project/app/src/main/java/com/droidcon/scandroid/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.droidcon.scandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.droidcon.scandroid.ui.theme.ScandroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScandroidTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
+

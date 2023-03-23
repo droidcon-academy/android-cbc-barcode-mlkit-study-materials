72c178f24d634c0e5cd5b0d546c9c9e72fd60f88
SubhrajyotiSen <subhrajyoti12@gmail.com>
2023-03-24 01:37:58 +0530

    Add source code

---
+++ source-code/Scandroid/app/src/main/java/com/droidcon/scandroid/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.droidcon.scandroid
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
+import androidx.camera.mlkit.vision.MlKitAnalyzer
+import androidx.camera.view.CameraController.COORDINATE_SYSTEM_VIEW_REFERENCED
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.droidcon.scandroid.ui.theme.ScandroidTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScandroidTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    CameraPreview()
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalPermissionsApi::class)
+    @Composable
+    fun CameraPreview() {
+        val cameraPermissionState = rememberPermissionState(
+            android.Manifest.permission.CAMERA
+        )
+        if (!cameraPermissionState.status.isGranted) {
+            LaunchedEffect(Unit) {
+                cameraPermissionState.launchPermissionRequest()
+            }
+        } else {
+            Column(modifier = Modifier.fillMaxSize()) {
+                AndroidView(
+                    modifier = Modifier.fillMaxSize(),
+                    factory = { getPreview() }
+                )
+            }
+        }
+    }
+
+
+    private fun getPreview(): PreviewView {
+        val cameraController = LifecycleCameraController(this)
+        val previewView = PreviewView(this)
+
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+            .build()
+        val barcodeScanner = BarcodeScanning.getClient(options)
+
+        cameraController.cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        cameraController.setImageAnalysisAnalyzer(
+            ContextCompat.getMainExecutor(this),
+            MlKitAnalyzer(
+                listOf(barcodeScanner),
+                COORDINATE_SYSTEM_VIEW_REFERENCED,
+                ContextCompat.getMainExecutor(this)
+            ) { result: MlKitAnalyzer.Result? ->
+                val barcodeResults = result?.getValue(barcodeScanner)
+                if (barcodeResults?.firstOrNull() == null) {
+                    previewView.overlay.clear()
+                    previewView.setOnTouchListener { _, _ -> false } //no-op
+                    return@MlKitAnalyzer
+                }
+
+                val firstResult = barcodeResults.first()
+                val drawable = QrCodeHighlightDrawable(firstResult.boundingBox!!)
+                previewView.overlay.clear()
+                previewView.overlay.add(drawable)
+                previewView.setOnTouchListener { v, event ->
+                    if (event.action == MotionEvent.ACTION_DOWN) {
+                        if (firstResult.boundingBox!!.contains(event.x.toInt(), event.y.toInt())) {
+                            if (firstResult.valueType == Barcode.TYPE_URL) {
+                                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(firstResult.url!!.url))
+                                startActivity(browserIntent)
+                            }
+                        }
+                    }
+                    true
+                }
+            }
+        )
+
+        cameraController.bindToLifecycle(this)
+        previewView.controller = cameraController
+        return previewView
+    }
+}
+
+
+
+
+
---
+++ source-code/Scandroid/app/src/main/java/com/droidcon/scandroid/QrCodeHighlightDrawable.kt
@@ -0,0 +1,36 @@
+package com.droidcon.scandroid
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.ColorFilter
+import android.graphics.Paint
+import android.graphics.PixelFormat
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+
+class QrCodeHighlightDrawable(
+    private val rect: Rect
+) : Drawable() {
+
+    private val paint = Paint().apply {
+        style = Paint.Style.STROKE
+        color = Color.WHITE
+        strokeWidth = 20F
+    }
+
+    override fun draw(canvas: Canvas) {
+        canvas.drawRect(rect, paint)
+    }
+
+    override fun setAlpha(alpha: Int) {
+        paint.alpha = alpha
+    }
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+        TODO("Not yet implemented")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun getOpacity(): Int = PixelFormat.OPAQUE
+
+}

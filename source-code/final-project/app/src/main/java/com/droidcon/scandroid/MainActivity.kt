cef916cc33d73033b186b5a6f3bb87250604e039
Sriyank Siddhartha <sriyank.siddhartha@gmail.com>
2024-05-06 17:00:34 +0530

    update final project

--- source-code/final-project/app/src/main/java/com/droidcon/scandroid/MainActivity.kt
+++ source-code/final-project/app/src/main/java/com/droidcon/scandroid/MainActivity.kt
@@ -12,13 +12,24 @@ import androidx.camera.mlkit.vision.MlKitAnalyzer
 import androidx.camera.view.CameraController.COORDINATE_SYSTEM_VIEW_REFERENCED
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.droidcon.scandroid.ui.theme.ScandroidTheme
@@ -28,92 +39,109 @@ import com.google.accompanist.permissions.rememberPermissionState
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
+import java.util.concurrent.Executors
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ScandroidTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    CameraPreview()
-                }
-            }
-        }
-    }
 
-    @OptIn(ExperimentalPermissionsApi::class)
-    @Composable
-    fun CameraPreview() {
-        val cameraPermissionState = rememberPermissionState(
-            android.Manifest.permission.CAMERA
-        )
-        if (!cameraPermissionState.status.isGranted) {
-            LaunchedEffect(Unit) {
-                cameraPermissionState.launchPermissionRequest()
-            }
-        } else {
-            Column(modifier = Modifier.fillMaxSize()) {
-                AndroidView(
-                    modifier = Modifier.fillMaxSize(),
-                    factory = { getPreview() }
-                )
-            }
-        }
-    }
+	private lateinit var cameraController: LifecycleCameraController
 
-    @SuppressLint("ClickableViewAccessibility")
-    private fun getPreview(): PreviewView {
-        val cameraController = LifecycleCameraController(this)
-        val previewView = PreviewView(this)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			ScandroidTheme {
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colors.background
+				) {
+					CameraPreview()
+				}
+			}
+		}
+	}
 
-        val options = BarcodeScannerOptions.Builder()
-            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
-            .build()
-        val barcodeScanner = BarcodeScanning.getClient(options)
+	@OptIn(ExperimentalPermissionsApi::class)
+	@Composable
+	fun CameraPreview() {
+		val cameraPermissionState = rememberPermissionState(
+			android.Manifest.permission.CAMERA
+		)
+		if (!cameraPermissionState.status.isGranted) {
+			LaunchedEffect(Unit) {
+				cameraPermissionState.launchPermissionRequest()
+			}
+		} else {
+			Box(modifier = Modifier.fillMaxSize()) {
+				AndroidView(
+					modifier = Modifier.fillMaxSize(),
+					factory = { getPreview() }
+				)
 
-        cameraController.cameraSelector = CameraSelector.Builder()
-            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-            .build()
+				var enableFlash by remember { mutableStateOf(false) }
+				val vectorAsset = if (enableFlash) {
+					R.drawable.flash_off
+				} else {
+					R.drawable.flash_on
+				}
+				Icon(
+					painterResource(id = vectorAsset), contentDescription = "Flash", modifier = Modifier
+						.align(Alignment.BottomCenter)
+						.padding(bottom = 24.dp)
+						.size(64.dp)
+						.clickable {
+							enableFlash = !enableFlash
+							cameraController.cameraControl?.enableTorch(enableFlash)
+						})
+			}
+		}
+	}
 
-        cameraController.setImageAnalysisAnalyzer(
-            ContextCompat.getMainExecutor(this),
-            MlKitAnalyzer(
-                listOf(barcodeScanner),
-                COORDINATE_SYSTEM_VIEW_REFERENCED,
-                ContextCompat.getMainExecutor(this)
-            ) { result: MlKitAnalyzer.Result? ->
-                val barcodeResults = result?.getValue(barcodeScanner)
-                if (barcodeResults?.firstOrNull() == null) {
-                    previewView.overlay.clear()
-                    previewView.setOnTouchListener { _, _ -> false } //no-op
-                    return@MlKitAnalyzer
-                }
+	@SuppressLint("ClickableViewAccessibility")
+	private fun getPreview(): PreviewView {
 
-                val firstResult = barcodeResults.first()
-                val drawable = QrCodeHighlightDrawable(firstResult.boundingBox!!)
-                previewView.overlay.clear()
-                previewView.overlay.add(drawable)
-                previewView.setOnTouchListener { _, event ->
-                    if (event.action == MotionEvent.ACTION_DOWN) {
-                        if (firstResult.boundingBox!!.contains(event.x.toInt(), event.y.toInt())) {
-                            if (firstResult.valueType == Barcode.TYPE_URL) {
-                                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(firstResult.url!!.url))
-                                startActivity(browserIntent)
-                            }
-                        }
-                    }
-                    true
-                }
-            }
-        )
+		val options = BarcodeScannerOptions.Builder()
+			.setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+			.build()
+		val barcodeScanner = BarcodeScanning.getClient(options)
 
-        cameraController.bindToLifecycle(this)
-        previewView.controller = cameraController
-        return previewView
-    }
+		cameraController = LifecycleCameraController(this)
+		val previewView = PreviewView(this)
+
+		cameraController.cameraSelector = CameraSelector.Builder()
+			.requireLensFacing(CameraSelector.LENS_FACING_BACK)
+			.build()
+
+		cameraController.setImageAnalysisAnalyzer(
+			Executors.newSingleThreadExecutor(),
+			MlKitAnalyzer(
+				listOf(barcodeScanner),
+				COORDINATE_SYSTEM_VIEW_REFERENCED,
+				ContextCompat.getMainExecutor(this)
+			) { result ->
+				val barcodeResults = result?.getValue(barcodeScanner)
+				if (barcodeResults?.firstOrNull() == null) {
+					previewView.overlay.clear()
+					previewView.setOnTouchListener { _, _ -> false } //no-op
+					return@MlKitAnalyzer
+				}
+
+				val firstResult = barcodeResults.first()
+				val drawable = QrCodeHighlightDrawable(firstResult.boundingBox!!)
+				previewView.overlay.clear()
+				previewView.overlay.add(drawable)
+				previewView.setOnTouchListener { _, event ->
+					if (event.action == MotionEvent.ACTION_DOWN) {
+						if (firstResult.boundingBox!!.contains(event.x.toInt(), event.y.toInt())) {
+							val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(firstResult.url!!.url))
+							startActivity(browserIntent)
+						}
+					}
+					true
+				}
+			}
+		)
+
+		cameraController.bindToLifecycle(this)
+		previewView.controller = cameraController
+		return previewView
+	}
 }
-
-
-
-
-
